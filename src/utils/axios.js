faf9c67286c6de84ed84a9468f2583bf8cc8a68d
masud467 <masudpfrnt@gmail.com>
2025-08-31 12:56:54 +0600

    added live server url.

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:9000",
+  // baseURL: "http://localhost:9000",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:9000",
 });
-export default axiosInstance
+export default axiosInstance;
